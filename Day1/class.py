ab0c75f3bbc86389174c304bcf3a32c0cd62dd97
sreevaishnavi-k <146219011+sreevaishnavi-k@users.noreply.github.com>
2023-10-09 16:30:31 +0530

    class.py

---
+++ Day1/class.py
@@ -0,0 +1,9 @@
+class siva:
+    def exsiva(bbroy,a):#bbroy is a userdefined key word instead of self 
+        print("hi",a)
+    def mrssiva(bbroy,b,c):
+        print("bye",b,c)
+child=siva()
+child.exsiva(10000)
+child.mrssiva(25000,'take care')
+
